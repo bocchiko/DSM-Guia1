731252d1dad51d12bf56182f01839718d7e4ef80
bocchiko <luisbonilla0908@gmail.com>
2024-02-08 14:12:38 -0600

    Desarrollo de Guia 1

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -1,43 +1,28 @@
 package com.example.practica1
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.practica1.ui.theme.Practica1Theme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+lateinit var nombre : EditText
+lateinit var enviar : Button
+lateinit var resultado : TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContent {
-            Practica1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        nombre = findViewById(R.id.TxtNombre)
+        enviar = findViewById(R.id.BtnEnviar)
+        resultado = findViewById(R.id.LlbResultado)
+
+        enviar.setOnClickListener{
+            var nombreUsuario: String = nombre.text.toString()
+            resultado.setText(nombreUsuario)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica1Theme {
-        Greeting("Android")
-    }
-}
