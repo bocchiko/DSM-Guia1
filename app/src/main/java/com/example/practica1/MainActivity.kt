bcb8e8c919bf2706b21d88fd0fc3f95f681c717e
bocchiko <luisbonilla0908@gmail.com>
2024-02-08 14:39:43 -0600

    Solucion a Ejercicio Complementario

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -6,8 +6,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-lateinit var nombre : EditText
-lateinit var enviar : Button
+lateinit var num1 : EditText
+lateinit var num2 : EditText
+lateinit var calcular : Button
 lateinit var resultado : TextView
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nombre = findViewById(R.id.TxtNombre)
-        enviar = findViewById(R.id.BtnEnviar)
+        num1 = findViewById(R.id.TxtNum1)
+        num2 = findViewById(R.id.TxtNum2)
+        calcular = findViewById(R.id.BtnCalcular)
         resultado = findViewById(R.id.LlbResultado)
 
-        enviar.setOnClickListener{
-            var nombreUsuario: String = nombre.text.toString()
-            resultado.setText(nombreUsuario)
+        calcular.setOnClickListener{
+            var TextoNumero1 = num1.text.toString()
+            var TextoNumero2 = num2.text.toString()
+
+            if (TextoNumero1.isNotEmpty() && TextoNumero2.isNotEmpty()){
+                var  numero1 = TextoNumero1.toFloat()
+                var  numero2 = TextoNumero2.toFloat()
+                var suma = numero1 + numero2
+
+                resultado.text = "Resultado: $suma"
+            }
+            else {
+                resultado.text = "Favor ingrese ambos numeros"
+            }
         }
     }
 }
